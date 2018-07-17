7f88a0f2fdfff957bb6292501dfe302b671c8bd2
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-07-17 09:59:12 +0000

    components: added base Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './index.css';
+
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+      </footer>
+    );
+  }
+}
+
+
+export default Footer;
