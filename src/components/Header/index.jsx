40459b81790b642c0b1bdbd139007b9febc4214e
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-07-17 09:58:46 +0000

    components: added base Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './index.css';
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+      </header>
+    );
+  }
+}
+
+
+export default Header;
