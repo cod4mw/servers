52d0c364f17a3a93810032e64c19f2ce9d9ba2e1
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-07-17 09:31:05 +0000

    gatsby: added gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Game Servers - Call of Duty 4: Modern Warfare',
+    description: 'List of Call of Duty 4: Modern Warfare game servers.'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet'
+  ]
+};
